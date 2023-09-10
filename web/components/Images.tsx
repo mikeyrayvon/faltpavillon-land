660a0013cc7dd2b671946d8ce6e6f673222aec64
Michael Ray-Von <michaelrayvon@gmail.com>
2023-09-10 16:54:47 +0200

    wrap images in container

--- web/components/Images.tsx
+++ web/components/Images.tsx
@@ -1,5 +1,6 @@
 import { urlFor } from "utils/sanity";
 import { Image } from "utils/types";
+import Container from "./Container";
 
 const Images = ({
   images,
@@ -9,22 +10,24 @@ const Images = ({
   defaultCaption: string;
 }) => {
   return (
-    <div className="flex flex-col items-center">
-      {images.map((image) => {
-        const { alt, caption, _key } = image;
-        return (
-          <figure className="mb-36" key={_key}>
-            <img
-              src={urlFor(image).url()}
-              alt={alt ? alt : caption ? caption : defaultCaption}
-            />
-            {caption && (
-              <figcaption className="text-white mt-2">{caption}</figcaption>
-            )}
-          </figure>
-        );
-      })}
-    </div>
+    <Container>
+      <div className="flex flex-col items-center">
+        {images.map((image) => {
+          const { alt, caption, _key } = image;
+          return (
+            <figure className="mb-20 lg:mb-24 xl:mb-36" key={_key}>
+              <img
+                src={urlFor(image).url()}
+                alt={alt ? alt : caption ? caption : defaultCaption}
+              />
+              {caption && (
+                <figcaption className="text-white mt-2">{caption}</figcaption>
+              )}
+            </figure>
+          );
+        })}
+      </div>
+    </Container>
   );
 };
 
